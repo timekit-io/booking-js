d644eaec26be5843617b50c43a106ce535e7f21e
Lasse Boisen Andersen <la@laander.com>
2015-11-11 11:21:31 +0100

    initial setup for tests using karma + jasmine

---
+++ test/main.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var timekitBooking = require('../dist/booking.js');
+
+/**
+ * Intilialise the library
+ */
+describe('Configuration', function() {
+
+  it('should be able initialize the library', function() {
+
+    expect(true).toEqual(true);
+
+  });
+
+});

2ad92b8dfd134723482e72b8c7cbf674525758fe
Lasse Boisen Andersen <la@laander.com>
2015-11-27 13:48:40 +0100

    made init tests for minified build

--- test/initialization.spec.js
+++ test/initialization.spec.js
@@ -6,7 +6,7 @@ var baseConfig = require('./utils/defaultConfig');
 var mockAjax = require('./utils/mockAjax');
 
 /**
- * Intilialize the library
+ * Intilialize the library with plain build
  */
 describe('Initialization regular', function() {
 
@@ -42,6 +42,42 @@ describe('Initialization regular', function() {
   it('should be able init and display the widget with singleton pattern', function() {
 
     TimekitBooking().init(baseConfig);
+
+    expect($('.bookingjs-calendar')).toBeInDOM();
+
+  });
+
+});
+
+/**
+ * Intilialize the library with minified build
+ */
+describe('Initialization minified', function() {
+
+  beforeEach(function(){
+    loadFixtures('minified.html');
+    mockAjax();
+  });
+
+  afterEach(function() {
+    jasmine.Ajax.uninstall();
+  });
+
+  it('should be able to load the fixture page', function() {
+
+    expect(window).toBeDefined();
+    expect($).toBeDefined();
+    expect($('#bookingjs')).toBeInDOM();
+
+  });
+
+  it('should be able init and display the widget with instance pattern', function() {
+
+    var widget = new TimekitBooking();
+    widget.init(baseConfig);
+
+    expect(widget).toBeDefined();
+    expect(widget.getConfig()).toBeDefined();
 
     expect($('.bookingjs-calendar')).toBeInDOM();
 

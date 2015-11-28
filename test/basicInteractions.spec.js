e162808bc670cf2cb2b04cbd46818f412b262414
Lasse Boisen Andersen <la@laander.com>
2015-11-28 01:00:59 +0100

    added test for booking flow

--- test/basicInteractions.spec.js
+++ test/basicInteractions.spec.js
@@ -47,4 +47,42 @@ describe('Basic interaction', function() {
 
   });
 
+  it('should be able to book an event', function(done) {
+
+    createWidget();
+
+    setTimeout(function() {
+
+      interact.clickEvent();
+
+      setTimeout(function() {
+
+        var name = 'Joe Test';
+        var email = 'test@timekit.io';
+        var comment = 'This is a test';
+
+        $('.input-name').val(name);
+        $('.input-email').val(email);
+        $('.input-comment').val(comment);
+        $('.bookingjs-form-button').click();
+
+        expect($('.bookingjs-form').hasClass('loading')).toBe(true);
+
+        setTimeout(function() {
+
+          expect($('.bookingjs-form').hasClass('success')).toBe(true);
+          expect($('.bookingjs-form-success-message')).toBeVisible();
+
+          var successMessage = $('.bookingjs-form-success-message').html();
+          var contains = successMessage.indexOf(email) > -1;
+          expect(contains).toBe(true);
+
+          done();
+
+        }, 100);
+      }, 500);
+    }, 500);
+
+  });
+
 });

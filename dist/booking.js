ee5af458b283bb47d9494821cf075cf0e1dc5b33
Lasse Boisen Andersen <la@laander.com>
2016-05-09 11:39:25 +0200

    build

--- dist/booking.js
+++ dist/booking.js
@@ -58,7 +58,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	/*!
 	 * Booking.js
-	 * Version: 2.0.0
+	 * Version: 1.8.0
 	 * http://timekit.io
 	 *
 	 * Copyright 2015 Timekit, Inc.

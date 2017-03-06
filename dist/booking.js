a38872d7f9e0080e8606242ab35eb3b78a49b608
Lasse Boisen Andersen <la@laander.com>
2017-03-06 17:36:01 +0100

    build

--- dist/booking.js
+++ dist/booking.js
@@ -142,10 +142,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      utils.doCallback('findTimeSuccessful', config, response);
 	
 	      // Render available timeslots in FullCalendar
-	      renderCalendarEvents(response.data);
-	
-	      // Go to first event if enabled
-	      if(config.goToFirstEvent && response.data.length > 0) goToFirstEvent(response.data[0].start);
+	      if(response.data.length > 0) renderCalendarEvents(response.data);
 	
 	    }).catch(function(response){
 	      utils.doCallback('findTimeFailed', config, response);
@@ -185,10 +182,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      utils.doCallback('getBookingSlotsSuccessful', config, response);
 	
 	      // Render available timeslots in FullCalendar
-	      renderCalendarEvents(slots);
-	
-	      // Go to first event if enabled
-	      if(config.goToFirstEvent && response.data.length > 0) goToFirstEvent(response.data[0].attributes.event_info.start);
+	      if(slots.length > 0) renderCalendarEvents(slots);
 	
 	    }).catch(function(response){
 	      utils.doCallback('getBookingSlotsFailed', config, response);
@@ -387,6 +381,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	    });
 	
 	    calendarTarget.removeClass('empty-calendar');
+	
+	    // Go to first event if enabled
+	    if (config.goToFirstEvent) goToFirstEvent(eventData[0].start);
 	
 	  };
 	

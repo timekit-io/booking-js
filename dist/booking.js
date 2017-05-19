03dc202e248c6cce124c02d3983ddc6381ac890f
Lasse Boisen Andersen <la@laander.com>
2017-05-19 17:08:13 +0200

    build with newest js-sdk

--- dist/booking.js
+++ dist/booking.js
@@ -1238,20 +1238,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	
 	  /**
-	   * Get user's Google calendars
-	   * @type {Function
-	   * @return {Promise}
-	   */
-	  TK.getAccountGoogleCalendars = function() {
-	
-	    return TK.makeRequest({
-	      url: '/accounts/google/calendars',
-	      method: 'get'
-	    });
-	
-	  };
-	
-	  /**
 	   * Initiate an account sync
 	   * @type {Function}
 	   * @return {Promise}
@@ -1260,21 +1246,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	    return TK.makeRequest({
 	      url: '/accounts/sync',
-	      method: 'get',
-	      params: data
-	    });
-	
-	  };
-	
-	  /**
-	   * Initiate an account sync where only calendar models are synced
-	   * @type {Function}
-	   * @return {Promise}
-	   */
-	  TK.accountSyncCalendars = function(data) {
-	
-	    return TK.makeRequest({
-	      url: '/accounts/sync/calendars',
 	      method: 'get',
 	      params: data
 	    });
@@ -1455,20 +1426,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	
 	  /**
-	   * Get users contacts that are present on Timekit (synced from providers)
-	   * @type {Function}
-	   * @return {Promise}
-	   */
-	  TK.getContacts = function() {
-	
-	    return TK.makeRequest({
-	      url: '/contacts/',
-	      method: 'get'
-	    });
-	
-	  };
-	
-	  /**
 	   * Get all user's events
 	   * @type {Function}
 	   * @return {Promise}
@@ -1545,21 +1502,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	
 	  /**
-	   * Get a user's anonymized availability (other user's on Timekit can be queryied by supplying their email)
-	   * @type {Function}
-	   * @return {Promise}
-	   */
-	  TK.getAvailability = function(data) {
-	
-	    return TK.makeRequest({
-	      url: '/events/availability',
-	      method: 'get',
-	      params: data
-	    });
-	
-	  };
-	
-	  /**
 	   * Find mutual availability across multiple users/calendars
 	   * @type {Function}
 	   * @return {Promise}
@@ -1575,7 +1517,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	
 	  /**
-	   * Find mutual availability across multiple users/calendars
+	   * Find bulk availability across multiple users/calendars
 	   * @type {Function}
 	   * @return {Promise}
 	   */
@@ -1584,6 +1526,68 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return TK.makeRequest({
 	      url: '/findtime/bulk',
 	      method: 'post',
+	      data: data
+	    });
+	
+	  };
+	
+	  /**
+	   * Find team availability across multiple users/calendars
+	   * @type {Function}
+	   * @return {Promise}
+	   */
+	  TK.findTimeTeam = function(data) {
+	
+	    return TK.makeRequest({
+	      url: '/findtime/team',
+	      method: 'post',
+	      data: data
+	    });
+	
+	  };
+	
+	  /**
+	   * Create a findtime filtercollection
+	   * @type {Function}
+	   * @return {Promise}
+	   */
+	  TK.createFindTimeFilterCollection = function(data) {
+	
+	    return TK.makeRequest({
+	      url: '/findtime/filtercollections',
+	      method: 'post',
+	      data: data
+	    });
+	
+	  };
+	
+	  /**
+	   * Get findtime filtercollections
+	   * @type {Function}
+	   * @return {Promise}
+	   */
+	  TK.getFindTimeFilterCollections = function() {
+	
+	    return TK.makeRequest({
+	      url: '/findtime/filtercollections',
+	      method: 'get'
+	    });
+	
+	  };
+	
+	  /**
+	   * Update a findtime filtercollections
+	   * @type {Function}
+	   * @return {Promise}
+	   */
+	  TK.updateFindTimeFilterCollection = function(data) {
+	
+	    var id = data.id;
+	    delete data.id;
+	
+	    return TK.makeRequest({
+	      url: '/findtime/filtercollections/' + id,
+	      method: 'get',
 	      data: data
 	    });
 	
@@ -1765,6 +1769,34 @@ return /******/ (function(modules) { // webpackBootstrap
 	      url: '/bookings/' + id + '/' + action,
 	      method: 'put',
 	      data: data
+	    });
+	
+	  };
+	
+	  /**
+	   * Get all bookings
+	   * @type {Function}
+	   * @return {Promise}
+	   */
+	  TK.getGroupBookings = function() {
+	
+	    return TK.makeRequest({
+	      url: '/bookings/groups',
+	      method: 'get'
+	    });
+	
+	  };
+	
+	  /**
+	   * Get specific booking
+	   * @type {Function}
+	   * @return {Promise}
+	   */
+	  TK.getGroupBooking = function(data) {
+	
+	    return TK.makeRequest({
+	      url: '/bookings/' + data.id + '/groups',
+	      method: 'get'
 	    });
 	
 	  };

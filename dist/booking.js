c804a3f7083e0a269998dfbf213fc75eec5b1f08
Lasse Boisen Andersen <la@laander.com>
2018-04-30 16:04:35 +0200

    flux release of v1.24.3

--- dist/booking.js
+++ dist/booking.js
@@ -891,7 +891,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  // Get library version
 	  var getVersion = function() {
 	
-	    return ("1.24.2");
+	    return ("1.24.3");
 	
 	  };
 	

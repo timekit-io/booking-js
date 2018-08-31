3e2f7836208ecb9893d0c4e90d5889efb9a82ee8
Lasse Boisen Andersen <la@laander.com>
2018-08-31 12:13:31 +0200

    flux release of v2.3.1

--- dist/booking.js
+++ dist/booking.js
@@ -257,7 +257,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	  // Get library version
 	  var getVersion = function() {
-	    return ("2.3.0");
+	    return ("2.3.1");
 	  };
 	
 	  var destroy = function() {

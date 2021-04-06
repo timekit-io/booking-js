a058784af09e565c73c1e23b50517e756dfcc284
Sandip Patel <sandip.patel@tulip.com>
2021-04-06 15:29:34 -0400

    final upgrades for booking-js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,62 @@
 'use strict';
 
 var webpack = require('webpack');
-var packageJson = require('./package.json')
+var packageJson = require('./package.json');
 
 module.exports = {
-    entry: './src/main.js',
-    devtool: 'source-map',
-    output: {
-        path: './dist',
-        filename: 'booking.js',
-        libraryTarget: 'umd',
-        library: 'TimekitBooking'
-    },
-    externals: {
-        'jquery': {
-            root: 'jQuery',
-            commonjs: 'jquery',
-            commonjs2: 'jquery',
-            amd: 'jquery'
-        }
-    },
-    module: {
-        loaders: [
-            { test: /\.html$/, loader: 'mustache?noShortcut' },
-            { test: /\.css$/, loaders: ['style?singleton', 'css?minimize', 'autoprefixer'] },
-            { test: /\.scss$/, loaders: ['style?singleton', 'css?minimize', 'autoprefixer', 'sass'] },
-            { test: /\.svg$/, loader: 'svg-inline' }
-        ]
-    },
-    plugins: [
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-gb/),
-        new webpack.DefinePlugin({
-          VERSION: JSON.stringify(packageJson.version)
-        }),
-        new webpack.ProvidePlugin({
-          Promise: 'es6-promise-promise'
-        })
-    ]
+	mode: 'development',
+	devtool: 'source-map',
+	entry: './src/main.js',
+	output: {
+		libraryTarget: 'umd',
+		filename: 'booking.js',
+		library: 'TimekitBooking',
+		path: __dirname + './dist',
+	},
+	externals: {
+		jquery: {
+			root: 'jQuery',
+			commonjs: 'jquery',
+			commonjs2: 'jquery',
+			amd: 'jquery',
+		},
+	},
+	module: {
+		rules: [
+			{ test: /\.html$/, loader: 'mustache-loader?noShortcut' },
+			{
+				test: /\.css$/,
+				loaders: [
+					{
+						loader: 'style-loader',
+						options: { injectType: 'singletonStyleTag' },
+					},
+					'css-loader',
+					'postcss-loader',
+				],
+			},
+			{
+				test: /\.scss$/,
+				loaders: [
+					{
+						loader: 'style-loader',
+						options: { injectType: 'singletonStyleTag' },
+					},
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+			{ test: /\.svg$/, loader: 'svg-inline-loader' },
+		],
+	},
+	plugins: [
+		new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-gb/),
+		new webpack.DefinePlugin({
+			VERSION: JSON.stringify(packageJson.version),
+		}),
+		new webpack.ProvidePlugin({
+			Promise: 'es6-promise-promise',
+		}),
+	],
 };

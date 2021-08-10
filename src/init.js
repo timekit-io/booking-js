d1d6b57c1e971d3c4780ab3ab00a7ade8220959b
Sandip Patel <sandip.patel@tulip.com>
2021-08-10 12:42:26 -0400

    Revert "css changes for booking widget"
    This reverts commit 1f130d31d4ae1cc4700af6682dfdb5ba616360d7.

--- src/init.js
+++ src/init.js
@@ -11,14 +11,11 @@ var RenderDep     = require('./render');
 function Initialize() {
 
   // SDK instance
-  var sdk     = timekitSdk.newInstance({
-    apiBaseUrl:"https://api-localhost.timekit.io/"
-  });
-
+  var sdk     = timekitSdk.newInstance();
   var config  = new ConfigDep();
-  var getConfig = config.retrieve;
   var utils   = new UtilsDep({ config: config });
   var render  = new RenderDep({ config: config, utils: utils, sdk: sdk });
+  var getConfig = config.retrieve;
 
   // Initilization method
   var init = function(suppliedConfig, global) {
@@ -29,7 +26,6 @@ function Initialize() {
 
     // Make sure that SDK is ready and debug flag is checked early
     var localConfig = config.setDefaults(suppliedConfig || {});
-
     config.update(localConfig);
     utils.logDebug(['Version:', getVersion()]);
     utils.logDebug(['Supplied config:', suppliedConfig]);

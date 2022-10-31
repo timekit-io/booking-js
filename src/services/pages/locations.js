2edd8c4e86bf72c02fb0ad68242aa82a00722e98
Sandip Patel <73543140+sandip-patel-1@users.noreply.github.com>
2022-10-31 12:27:29 -0400

    fixed issue with geo location

--- src/services/pages/locations.js
+++ src/services/pages/locations.js
@@ -10,6 +10,8 @@ const SearchIcon = require('!file-loader!../assets/icon_search.svg').default;
 class LocationsPage extends BaseTemplate {
     constructor(template) {
         super(template);
+        this.latitude = 0;
+        this.longitude = 0;
         this.sdk = template.sdk;
         this.template = template;
         this.utils = template.utils;
@@ -17,10 +19,37 @@ class LocationsPage extends BaseTemplate {
         this.config = template.config;
     }
 
+    getDegreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    getDistanceInKmBetweenEarthCoordinates(latitude, longitude) {
+        const earthRadiusKm = 6371;
+        
+        let destLatitudeInRadians = this.getDegreesToRadians(this.latitude - latitude);
+        let destLongitudeInRadians = this.getDegreesToRadians(this.longitude - longitude);
+        
+        latitude = this.getDegreesToRadians(latitude);
+        let latitudeInRadiants = this.getDegreesToRadians(this.latitude);
+        
+        let a = Math.sin(destLatitudeInRadians/2) * Math.sin(destLatitudeInRadians/2) + Math.sin(destLongitudeInRadians/2) * Math.sin(destLongitudeInRadians/2) * Math.cos(latitude) * Math.cos(latitudeInRadiants); 
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return parseFloat((earthRadiusKm * c).toFixed(1));
+    }
+
+    getDistance(project) {
+        return this.getDistanceInKmBetweenEarthCoordinates(get(project, 'meta.t_latitude') || 0, get(project, 'meta.t_longitude') || 0);
+    }
+
     getLocationsTemplate(locations) {
         const template = require('../templates/slots/locations.html');
         return this.htmlToElement(template({
-            locations: locations
+            selectorOptions: this.config.get('selectorOptions.location'),
+            locations: locations.map((projectData) => ({
+                ...projectData,
+                distance: this.getDistance(projectData)
+            }))
         }));
     }
 
@@ -65,8 +94,21 @@ class LocationsPage extends BaseTemplate {
 
         const searchInput = this.template.pageTarget.querySelector('#search-bar');
         const locationsEle = this.template.pageTarget.querySelector('#location-list');
+        const searchGeoLocBtn = this.template.pageTarget.querySelector('#geolocation-search');
 
         locationsEle.append(this.locationsTarget);
+
+        searchInput && searchInput.addEventListener("focus", (e) => {
+            if (this.config.get('selectorOptions.location.search_bar.enabled')) {
+                searchGeoLocBtn && searchGeoLocBtn.classList.remove('hide');
+            }
+            e.preventDefault();
+        });
+
+        searchGeoLocBtn && searchGeoLocBtn.addEventListener("focusout", (e) => {
+            e.target.classList.add('hide');
+            e.preventDefault();
+        });
 
         searchInput && searchInput.addEventListener("input", (e) => {
             const filteredLocations = filter(locations, function({ meta }) { 
@@ -83,6 +125,35 @@ class LocationsPage extends BaseTemplate {
             this.initLocationClick(filteredLocations, serviceId);
 
             e.preventDefault();
+        });
+
+        searchGeoLocBtn && searchGeoLocBtn.addEventListener("click", async (e) => {
+            e.preventDefault();
+            e.target.classList.add('hide');
+            return await new Promise(async (resolve) => {
+                const options = {
+                    timeout: 5000,
+                    enableHighAccuracy: false,
+                    maximumAge: 600000 // Keep cached data for 10 mins
+                };
+                return await window.navigator.geolocation.getCurrentPosition((position) => {
+                    this.latitude = position.coords.latitude;
+                    this.longitude = position.coords.longitude;
+                    return resolve({});
+                }, () => {
+                    this.latitude = null;
+                    this.longitude = null;
+                    return resolve({});
+                }, options);
+            }).then(() => {
+                locations.sort((prev, next) => prev.distance - next.distance);
+
+                locationsEle.removeChild(this.locationsTarget);
+                this.locationsTarget = this.getLocationsTemplate(locations);
+                
+                locationsEle.append(this.locationsTarget);
+                this.initLocationClick(locations, serviceId);
+            })
         });
 
         this.initLocationClick(locations, serviceId);
